9d75cd7afe9985e17d29b26179c8589869bd4f7e
meghanlo <lo.meghan.e@gmail.com>
2021-05-07 23:14:07 -0400

    small update to model and schema to reflect current state

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require 'uuid'
+include(Rails.application.routes.url_helpers)
 
 class Image < ApplicationRecord
   validates :name, presence: true
   validates :canonical_id, presence: true
-  validates :image_url, presence: true
 
   has_many :tags, class_name: 'ImageTag', inverse_of: :image, dependent: :restrict_with_exception
+
+  has_one_attached :image_file, dependent: :destroy
 
   before_validation :assign_canonical_id!
 
