c8e16981ec07c3ae130098d0183249be354b731f
meghanlo <lo.meghan.e@gmail.com>
2021-05-09 12:40:44 -0400

    adding delete and bulk delete endpoint

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_image_mutation, mutation: Mutations::CreateImageMutation
     field :update_image_mutation, mutation: Mutations::UpdateImageMutation
+    field :delete_image_mutation, mutation: Mutations::DeleteImageMutation
+    field :delete_many_image_mutation, mutation: Mutations::DeleteManyImageMutation
   end
 end
